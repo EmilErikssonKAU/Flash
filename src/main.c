a879c5e589313f6a34acf659ede3024cb82b3a75
Emil Eriksson <emileriksson432@gmail.com>
2026-02-09 21:45:18 +0100

    Added print-eval loop.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_INPUT_LENGTH 100
 
 int main(int argc, char *argv[])
 {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-  printf("$ ");
-
+  setbuf(stdout, NULL); // Flush after every printf
   char buf[1024];
-  fgets(buf, MAX_INPUT_LENGTH, stdin);
 
-  // Remove newline
-  buf[strlen(buf) - 1] = '\0';
-  printf("%s: command not found\n", buf);
+  // Read-Eval-Print Loop
+  while (true)
+  {
+    printf("$ ");
+    fgets(buf, MAX_INPUT_LENGTH, stdin);
+    buf[strlen(buf) - 1] = '\0'; // Remove newline
+    printf("%s: command not found\n", buf);
+  }
 
   return 0;
 }
