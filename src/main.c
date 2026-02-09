391eee3ff043a3294cdef9fd5e0b237569ce10d5
Emil Eriksson <emileriksson432@gmail.com>
2026-02-09 21:39:20 +0100

    Shell reads one line of input and warns if command is not found. No command can currently be found.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT_LENGTH 100
 
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  char buf[1024];
+  fgets(buf, MAX_INPUT_LENGTH, stdin);
+
+  // Remove newline
+  buf[strlen(buf) - 1] = '\0';
+  printf("%s: command not found\n", buf);
 
   return 0;
 }
