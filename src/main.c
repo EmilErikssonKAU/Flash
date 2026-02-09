aee85685bf7dec6dd23717f4a36572452ae082cf
Emil Eriksson <emileriksson432@gmail.com>
2026-02-09 21:56:32 +0100

    Added exit condition to loop.

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include <stdbool.h>
 
 #define MAX_INPUT_LENGTH 100
+#define BUF_SIZE 1024
 
 int main(int argc, char *argv[])
 {
   setbuf(stdout, NULL); // Flush after every printf
-  char buf[1024];
+  char buf[BUF_SIZE];
 
   // Read-Eval-Print Loop
   while (true)
@@ -16,6 +17,8 @@ int main(int argc, char *argv[])
     printf("$ ");
     fgets(buf, MAX_INPUT_LENGTH, stdin);
     buf[strlen(buf) - 1] = '\0'; // Remove newline
+    if (!strcmp(buf, "exit"))
+      break;
     printf("%s: command not found\n", buf);
   }
 
