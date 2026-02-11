d08e96be62d54db4189e63cfc8d1f6381b78a4f5
Emil Eriksson <emileriksson432@gmail.com>
2026-02-11 19:25:45 +0100

    Fixed small bug in output of type command

--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
       }
       else
       {
-        printf("invalid_command: not found\n");
+        printf("%s: not found\n", second_lex);
       }
     }
     else
