831be5248e5cd7e06dba54f3a6346ca1968f94cc
Emil Eriksson <emileriksson432@gmail.com>
2026-02-14 13:33:19 +0100

    Added support for ~ path expansion.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 #define BUF_SIZE 1024
 #define MAX_ARGS 10
 
-char *extractPath(char *PATH)
+char *extractPath(const char *PATH)
 {
   char *buf;
   if (PATH != NULL)
@@ -24,7 +24,7 @@ char *extractPath(char *PATH)
   return path_section;
 }
 
-bool checkPath(char *lexeme, bool should_print)
+bool checkPath(const char *lexeme, bool should_print)
 {
   char *PATH = getenv("PATH");
   char *path_section = extractPath(PATH);
@@ -41,6 +41,26 @@ bool checkPath(char *lexeme, bool should_print)
     path_section = extractPath(NULL);
   }
   return false;
+}
+
+char *expand_path(const char *path)
+{
+  if (!path)
+    return NULL;
+
+  if (path[0] != '~')
+  {
+    char *expansion = malloc(strlen(path) + 1);
+    memcpy(expansion, path, strlen(path) + 1);
+    return expansion;
+  }
+
+  const char *home = getenv("HOME");
+
+  size_t expanded_path_len = strlen(home) + strlen(path) + 1;
+  char *expanded_path = malloc(expanded_path_len);
+  snprintf(expanded_path, expanded_path_len, "%s%s", home, path + 1);
+  return expanded_path;
 }
 
 void build_argv(char *prog_name, char *argv[])
@@ -144,10 +164,12 @@ int main(int argc, char *argv[])
       }
       else
       {
-        if (chdir(second_lex))
+        char *expanded_path = expand_path(second_lex);
+        if (chdir(expanded_path))
         {
           printf("cd: %s: No such file or directory\n", second_lex);
         }
+        free(expanded_path);
       }
     }
     else if (checkPath(first_lex, false))
