78e7e270a7c5cda634faf888b63e84d27eaa215a
Emil Eriksson <emileriksson432@gmail.com>
2026-02-20 12:13:04 +0100

    Created first AST structure

---
+++ src/ast.h
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdint.h>
+
+typedef enum
+{
+    REDIR_STDOUT,
+    REDIR_FD1,
+    REDIR_FD2
+} RedirKind;
+
+typedef struct
+{
+    RedirKind kind;
+    char *target;
+} Redir;
+
+typedef struct
+{
+    char **v;
+    size_t n;
+} StrVec;
+
+typedef struct
+{
+    Redir *v;
+    size_t n;
+} RedirVec;
+
+typedef struct
+{
+    StrVec argv;
+    RedirVec redirs;
+} AstCommand;
+
+typedef struct AstNode
+{
+    AstCommand cmd;
+} AstNode;
