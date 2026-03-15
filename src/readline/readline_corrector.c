3bc163824757ffd4e206e897c9e549150fd389f3
Emil Eriksson <emileriksson432@gmail.com>
2026-03-15 13:24:43 +0100

    Positioning at promptline fixed.

--- src/readline/readline_corrector.c
+++ src/readline/readline_corrector.c
@@ -3,18 +3,23 @@
 
 int readline_corrector()
 {
+    static int test = 1;
     // Move down cursor one row
     move_cursor_down();
 
-    // // // Clear line
+    // Clear line
     clear_line();
 
-    // // // Print correction suggestion
-    printf("\rNew suggestion!");
+    // Print correction suggestion
+    printf("\rNew suggestion!%d", test % 10);
 
-    // // Move cursor back up one row
+    // Move cursor back to start of line;
+    printf("\r");
+
+    // Move cursor back up one row
     move_cursor_up();
 
     // Move cursor correct amount of steps forward
     move_cursor_end_of_prompt();
+    test++;
 }
